efd2d7b928105ef0854590bbb988edbf34366609
ja50n <zs_feng@qq.com>
2023-03-21 21:25:41 +0800

    comments

--- include/core/primitives/Aggregate.h
+++ include/core/primitives/Aggregate.h
@@ -8,13 +8,16 @@
  */
 
 namespace TRay {
-/// @brief Other virtuals are to be implemented by concrete acceleration structures.
 class Aggregate : public Primitive {
  private:
   AreaLight *do_area_light() const override;
   Material *do_material() const override;
   void do_fill_scattering_func(SurfaceInteraction *si, TransportMode mode,
                                bool allow_multi_lobes) const override;
+  /// @brief Other virtuals are to be implemented by concrete structures:
+  // Bound3f do_world_bound() const override;
+  // bool do_intersect(const Ray &ray, SurfaceInteraction *si) const override;
+  // bool do_intersect_test(const Ray &ray) const override;
 };
 
 }  // namespace TRay
