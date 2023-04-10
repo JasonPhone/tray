c1fd412cc962eb38d9e478cc71996dd80b243707
ja50n <zs_feng@qq.com>
2023-04-10 21:37:12 +0800

    fix: - (Sphere.cpp) Revisited intersect() and forward interset_test() to it.

--- src/shapes/Sphere.cpp
+++ src/shapes/Sphere.cpp
@@ -17,6 +17,9 @@ Bound3f Sphere::object_bound() const {
  */
 bool Sphere::intersect(const Ray &ray, Float *time, SurfaceInteraction *si,
                        bool test_alpha_texture) const {
+  // SDebug("Sphere::intersect: testing ray " + ray.to_string());
+  // SDebug("\twith sphere at " + obj_to_world(Point3f(0, 0, 0)).to_string() +
+  //        ", radius " + format_one("%f ", radius));
   Float phi;
   Point3f p_hit;
   // Ray to obj space.
@@ -40,22 +43,25 @@ bool Sphere::intersect(const Ray &ray, Float *time, SurfaceInteraction *si,
   p_hit = obj_ray(t_hit);
   // Refine.
   p_hit *= radius / distance(p_hit, Point3f(0, 0, 0));
-  if (p_hit.x == 0 && p_hit.y == 0) p_hit.x *= 1e-5f * radius;
+  // Avoid 0 in following z_radius.
+  if (p_hit.x == 0 && p_hit.y == 0) p_hit.x = 1e-5 * radius;
   phi = std::atan2(p_hit.y, p_hit.x);
   if (phi < 0) phi += 2 * PI;
-  Float theta = std::acos(clamp(p_hit.z, -1, 1));
+  Float theta = std::acos(clamp(p_hit.z / radius, -1, 1));
   // Calculate uv.
   Float u = phi / phi_max;
-  Float v = (theta - theta_min) / (theta_max - theta_min);
+  // Float v = (theta - theta_min) / (theta_max - theta_min);
+  Float v = (theta - theta_max) / (theta_min - theta_max);
   // Compute sphere dpdu and dpdv
   Float z_radius = std::sqrt(p_hit.x * p_hit.x + p_hit.y * p_hit.y);
   Float z_radius_inv = 1 / z_radius;
-  Float cosPhi = p_hit.x * z_radius_inv;
-  Float sinPhi = p_hit.y * z_radius_inv;
+  Float cos_phi = p_hit.x * z_radius_inv;
+  Float sin_phi = p_hit.y * z_radius_inv;
   Vector3f dpdu(-phi_max * p_hit.y, phi_max * p_hit.x, 0);
   Vector3f dpdv =
-      (theta_max - theta_min) *
-      Vector3f(p_hit.z * cosPhi, p_hit.z * sinPhi, -radius * std::sin(theta));
+      // (theta_max - theta_min) *
+      (theta_min - theta_max) *
+      Vector3f(p_hit.z * cos_phi, p_hit.z * sin_phi, -radius * std::sin(theta));
   // Return values.
   *time = t_hit;
   *si = obj_to_world(SurfaceInteraction(p_hit, Point2f(u, v), -ray.dir, dpdu,
@@ -64,47 +70,50 @@ bool Sphere::intersect(const Ray &ray, Float *time, SurfaceInteraction *si,
 }
 /// @brief Same as intersect() but does not update interaction.
 bool Sphere::intersect_test(const Ray &ray, bool test_alpha_texture) const {
-  Float phi;
-  Point3f p_hit;
-  // Ray to obj space.
-  Ray obj_ray = world_to_obj(ray);
-  // Construct formula.
-  Float dx = obj_ray.dir.x, dy = obj_ray.dir.y, dz = obj_ray.dir.z;
-  Float ox = obj_ray.ori.x, oy = obj_ray.ori.y, oz = obj_ray.ori.z;
-  Float a = dx * dx + dy * dy + dz * dz;
-  Float b = 2 * (dx * ox + dy * oy + dz * oz);
-  Float c = ox * ox + oy * oy + oz * oz - radius * radius;
-  // Solve formula.
-  Float t0, t1;
-  if (!solve_quadratic(a, b, c, &t0, &t1)) return false;
-  // The time range.
-  if (t0 > obj_ray.t_max || t1 <= 0) return false;
-  Float t_hit = t0;
-  if (t_hit <= 0) {
-    t_hit = t1;
-    if (t_hit > obj_ray.t_max) return false;
-  }
-  p_hit = obj_ray(t_hit);
-  // Refine.
-  p_hit *= radius / distance(p_hit, Point3f(0, 0, 0));
-  if (p_hit.x == 0 && p_hit.y == 0) p_hit.x *= 1e-5f * radius;
-  phi = std::atan2(p_hit.y, p_hit.x);
-  if (phi < 0) phi += 2 * PI;
-  Float theta = std::acos(clamp(p_hit.z, -1, 1));
-  // Calculate uv.
-  Float u = phi / phi_max;
-  Float v = (theta - theta_min) / (theta_max - theta_min);
-  // Compute sphere dpdu and dpdv
-  Float z_radius = std::sqrt(p_hit.x * p_hit.x + p_hit.y * p_hit.y);
-  Float z_radius_inv = 1 / z_radius;
-  Float cosPhi = p_hit.x * z_radius_inv;
-  Float sinPhi = p_hit.y * z_radius_inv;
-  Vector3f dpdu(-phi_max * p_hit.y, phi_max * p_hit.x, 0);
-  Vector3f dpdv =
-      (theta_max - theta_min) *
-      Vector3f(p_hit.z * cosPhi, p_hit.z * sinPhi, -radius * std::sin(theta));
-
-  return true;
+  SurfaceInteraction si;
+  Float time;
+  return this->intersect(ray, &time, &si, test_alpha_texture);
+  // Float phi;
+  // Point3f p_hit;
+  // // Ray to obj space.
+  // Ray obj_ray = world_to_obj(ray);
+  // // Construct formula.
+  // Float dx = obj_ray.dir.x, dy = obj_ray.dir.y, dz = obj_ray.dir.z;
+  // Float ox = obj_ray.ori.x, oy = obj_ray.ori.y, oz = obj_ray.ori.z;
+  // Float a = dx * dx + dy * dy + dz * dz;
+  // Float b = 2 * (dx * ox + dy * oy + dz * oz);
+  // Float c = ox * ox + oy * oy + oz * oz - radius * radius;
+  // // Solve formula.
+  // Float t0, t1;
+  // if (!solve_quadratic(a, b, c, &t0, &t1)) return false;
+  // // The time range.
+  // if (t0 > obj_ray.t_max || t1 <= 0) return false;
+  // Float t_hit = t0;
+  // if (t_hit <= 0) {
+  //   t_hit = t1;
+  //   if (t_hit > obj_ray.t_max) return false;
+  // }
+  // p_hit = obj_ray(t_hit);
+  // // Refine.
+  // p_hit *= radius / distance(p_hit, Point3f(0, 0, 0));
+  // if (p_hit.x == 0 && p_hit.y == 0) p_hit.x = 1e-5f * radius;
+  // phi = std::atan2(p_hit.y, p_hit.x);
+  // if (phi < 0) phi += 2 * PI;
+  // Float theta = std::acos(clamp(p_hit.z, -1, 1));
+  // // Calculate uv.
+  // Float u = phi / phi_max;
+  // Float v = (theta - theta_max) / (theta_min - theta_max);
+  // // Compute sphere dpdu and dpdv
+  // Float z_radius = std::sqrt(p_hit.x * p_hit.x + p_hit.y * p_hit.y);
+  // Float z_radius_inv = 1 / z_radius;
+  // Float cosPhi = p_hit.x * z_radius_inv;
+  // Float sinPhi = p_hit.y * z_radius_inv;
+  // Vector3f dpdu(-phi_max * p_hit.y, phi_max * p_hit.x, 0);
+  // Vector3f dpdv =
+  //     (theta_min - theta_max) *
+  //     Vector3f(p_hit.z * cosPhi, p_hit.z * sinPhi, -radius *
+  //     std::sin(theta));
+  // return true;
 }
 Interaction Sphere::sample_surface(const Point2f &u, Float *pdf_value) const {
   Point3f p_surface = Point3f(0, 0, 0) + radius * sphere_uniform_sample(u);
