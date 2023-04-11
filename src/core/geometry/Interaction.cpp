a0064e5629d21d365e844c34de280616f01b4bc0
ja50n <zs_feng@qq.com>
2023-04-11 19:50:31 +0800

    accounting for flipping normal and handness

--- src/core/geometry/Interaction.cpp
+++ src/core/geometry/Interaction.cpp
@@ -1,5 +1,6 @@
 #include "core/geometry/Interaction.h"
 #include "core/primitives/Primitive.h"
+#include "core/geometry/Shape.h"
 #include "core/Light.h"
 
 namespace TRay {
@@ -16,13 +17,26 @@ SurfaceInteraction::SurfaceInteraction(const Point3f &p, const Point2f &uv,
   shading.n = n;
   shading.dpdu = dpdu;
   shading.dpdv = dpdv;
+  // The intersection is done in left-handed space, adjust if needed.
+  if (shape && (shape->flip_normal ^ shape->swap_handness)) {
+    n *= -1;
+    shading.n *= -1;
+  }
 }
-void SurfaceInteraction::fill_scattering_func(const Ray &ray, bool allow_multi_lobes,
+void SurfaceInteraction::fill_scattering_func(const Ray &ray,
+                                              bool allow_multi_lobes,
                                               TransportMode mode) {
   primitive->fill_scattering_func(this, mode, allow_multi_lobes);
 }
 Spectrum SurfaceInteraction::Le(const Vector3f &w) const {
   const AreaLight *light = primitive->area_light();
+  // if (light) {
+  //   // SDebug("this primitive has an area light");
+  //   return light->L(*this, w);
+  // } else {
+  //   // SDebug("this primitive does not have area light");
+  //   return Spectrum(0.0);
+  // }
   return light ? light->L(*this, w) : Spectrum(0.0);
 }
 }  // namespace TRay
