e3e988c46ee2b87a264843204616136334267005
ja50n <zs_feng@qq.com>
2023-04-27 16:15:25 +0800

    TRay 1.0.1?

--- apps/TRay/main.cpp
+++ apps/TRay/main.cpp
@@ -2,6 +2,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "ImGui/imgui.h"
+#include "ImGui/backend/imgui_impl_glfw.h"
+#include "ImGui/backend/imgui_impl_opengl3.h"
+#include "gui/utility.h"
+#include "gui/Shader.h"
+#include "file_dialog/file_dialog.h"
+
 #include <iostream>
 #include "core/TRay.h"
 #include "core/imageio.h"
@@ -26,22 +33,264 @@ using namespace TRay;
 using namespace std;
 
 uint8_t image[1000 * 1000 * 3];
+const int window_w = 1280, window_h = 720;
+int image_w = 200, image_h = 200;
+
+double last_render_time = 0;
+std::string file_path;
+SceneLoader sloader;
 void render_file(const char*);
+
+static void glfw_error_callback(int error, const char* description) {
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+  printf("Glfw Error %d: %s\n", error, description);
+}
+
+static bool open_scene_file(const char* path) {
+  file_path = path;
+  bool stat = sloader.reload(path);
+  if (stat) {
+    image_w = sloader.get_camera()->m_film->m_full_resolution.x;
+    image_h = sloader.get_camera()->m_film->m_full_resolution.y;
+    return true;
+  } else {
+    SError("Error loading scene file.");
+    return false;
+  }
+}
+static void save_image() {
+  if (sloader.get_camera()) {
+    string name = sloader.get_camera()->m_film->m_filename;
+    stbi_write_jpg(name.c_str(), image_w, image_h, 3, image, 95);
+  }
+}
 
 int main(int argc, char* argv[]) {
   fill(image, image + sizeof(image), 0);
+
   if (argc > 1) {
-    render_file(argv[1]);
+    for (int i = 1; i < argc; i++) render_file(argv[i]);
+    return 0;
   }
-  // Process command-line args.
-  // Init from command-line args.
-  // Read from json, build scene.
-  // Clean up.
+  for (int i = 0; i < image_h; i++) {
+    for (int j = 0; j < image_w; j++) {
+      image[(i * image_w + j) * 3 + 0] = 1.0 * i / image_h * 255;
+      image[(i * image_w + j) * 3 + 1] = 1.0 * j / image_w * 255;
+      image[(i * image_w + j) * 3 + 2] = 255;
+    }
+  }
+
+  // Setup window
+  printf("setup\n");
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit()) return 1;
+
+  // Decide GL+GLSL versions
+  // GL 3.0 + GLSL 130
+  printf("choose version\n");
+  const char* glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE,
+                 GLFW_OPENGL_CORE_PROFILE);  // use core profile
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  // Create window with graphics context
+  printf("create window context\n");
+  GLFWwindow* window = glfwCreateWindow(window_w, window_h, "TRay", NULL, NULL);
+  if (window == NULL) return 1;
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);  // Enable vsync
+
+  // Initialize OpenGL loader
+  bool err = gladLoadGL() == 0;
+  if (err) {
+    fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+    return 1;
+  }
+
+  // Setup Dear ImGui context
+  printf("imgui context\n");
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+
+  printf("binding\n");
+  // Setup Platform/Renderer bindings
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+
+  // Rander result as background texture.
+  Shader shader{"./shader.vs", "./shader.fs"};
+  // vao and vbo
+  // clang-format off
+  float unique_vert[] = {
+  // ---- position ---- - tex coord -
+     0.8f,  0.8f, 0.0f, 1.0f, 0.0f,
+     0.8f, -0.8f, 0.0f, 1.0f, 1.0f,
+    -0.8f, -0.8f, 0.0f, 0.0f, 1.0f,
+    -0.8f,  0.8f, 0.0f, 0.0f, 0.0f
+  };
+  GLuint indices[] = {0, 1, 3, 1, 2, 3};
+  // clang-format on
+  GLuint EBO, VAO, VBO;
+  glGenBuffers(1, &EBO);
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  // buffer VBO
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(unique_vert), unique_vert,
+               GL_STATIC_DRAW);
+  // vertex attrib: position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  // vertex attrib: color
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                        (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // buffer EBO
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
+
+  // render loop
+  shader.use();
+  shader.set_int("texture0", 0);
+  GLuint texture0 = 0;
+  texture0 = create_texture(image, image_w, image_h);
+
+  printf("main loop\n");
+  // Main loop
+  // GUI state.
+  bool file_opened = false;
+  bool file_loaded = false;
+  bool rendering = false;
+  bool m_fileDialogOpen;
+  ImFileDialogInfo m_fileDialogInfo;
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.3, 0.3, 0.3, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Update image.
+    double time = glfwGetTime();
+    if (rendering && time - last_render_time > 0.03) {
+      sloader.get_integrator()->render_step(*(sloader.get_scene()));
+      sloader.get_camera()->m_film->write_image(1.0, image);
+      glDeleteTextures(1, &texture0);
+      texture0 = create_texture(image, image_w, image_h);
+      last_render_time = time;
+    }
+
+    // Redraw image.
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture0);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    {
+      ImGui::Begin("Panel");
+
+      // Open file.
+      if (ImGui::Button("Open")) {
+        rendering = false;
+        // Open file callback.
+        m_fileDialogOpen = true;
+        m_fileDialogInfo.type = ImGuiFileDialogType_OpenFile;
+        m_fileDialogInfo.title = "Open Scene File";
+        m_fileDialogInfo.fileName = "test.json";
+        m_fileDialogInfo.directoryPath = std::filesystem::current_path();
+      }
+      if (ImGui::FileDialog(&m_fileDialogOpen, &m_fileDialogInfo)) {
+        // Result path in: m_fileDialogInfo.resultPath
+        file_opened = file_loaded =
+            open_scene_file(m_fileDialogInfo.resultPath.string().c_str());
+      }
+      ImGui::SameLine();
+      if (file_path.empty())
+        ImGui::Text("No active scene file.");
+      else
+        ImGui::Text("Scene file: %s", file_path.c_str());
+
+      // Reload file.
+      if (ImGui::Button("Reload")) {
+        if (file_opened) {
+          file_loaded = open_scene_file(file_path.c_str());
+          rendering = false;
+        }
+      }
+      ImGui::SameLine();
+      if (file_opened) {
+        if (file_loaded)
+          ImGui::Text("File loaded.");
+        else
+          ImGui::Text("File not loaded.");
+      } else {
+        ImGui::Text("No active scene file.");
+      }
+      // Render.
+      if (ImGui::Button("Render")) rendering = !rendering;
+      ImGui::SameLine();
+      if (rendering)
+        ImGui::Text("Rendering.");
+      else
+        ImGui::Text("Paused.");
+
+      // Export image.
+      if (ImGui::Button("Export")) {
+        save_image();
+      }
+      ImGui::SameLine();
+      if (sloader.get_camera()) {
+        ImGui::Text("%s", sloader.get_camera()->m_film->m_filename.c_str());
+      }
+
+      ImGui::Text("%.3f ms/frame (%.1f FPS)",
+                  1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      ImGui::End();
+    }
+
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    int view_h = std::min(image_h * 2.0, window_h * 0.8);
+    int view_w = 1.0 * image_w / image_h * view_h;
+    glViewport((display_w - view_w) / 2, (display_h - view_h) / 2, view_w,
+               view_h);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+
+  printf("clean\n");
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return 0;
 }
 
 void render_file(const char* path) {
-  SceneLoader sloader{path};
   if (!sloader.reload(path)) {
     SError("Error loading scene file " + string(path));
     return;
